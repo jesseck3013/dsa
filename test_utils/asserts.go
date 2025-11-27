32028e8a79f1040dee9c2e93760591ffceff6d9a
jesseck3013 <jesseck3013@outlook.com>
2025-11-27 15:22:31 +0800

    Fixed: call t.Helper() in asserts

--- test_utils/asserts.go
+++ test_utils/asserts.go
@@ -3,19 +3,21 @@ package testutils
 import "testing"
 
 func AssertNoError(t *testing.T, err error) {
+	t.Helper()
 	if err != nil {
-		t.Error(err)
-		panic("expected no err")
+		t.Fatalf("expected no error, but got %v", err)
 	}
 }
 
 func AssertError(t *testing.T, want, got error) {
+	t.Helper()
 	if want != got {
 		t.Errorf("expected error: %v, got %v", want, got)
 	}
 }
 
 func AssertValue[T comparable](t *testing.T, want, got T) {
+	t.Helper()
 	if got != want {
 		t.Errorf("expected %v, got %v", want, got)
 	}
