797a82708fd1c466d4bd0336c309988302fb0b51
jesseck3013 <jesseck3013@outlook.com>
2025-11-27 15:35:10 +0800

    Added: stack implementation

--- container/stack.go
+++ container/stack.go
@@ -1,1 +1,51 @@
 package container
+
+type StackErr string
+
+func (s StackErr) Error() string {
+	return string(s)
+}
+
+const (
+	ErrEmpty = StackErr("Stack is empty")
+)
+
+type Stack[T any] struct {
+	store []T
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
+		store: make([]T, 0),
+	}
+}
+
+func (s *Stack[T]) Empty() bool {
+	return len(s.store) == 0
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.store = append(s.store, value)
+}
+
+func (s *Stack[T]) Top() (T, error) {
+	if s.Empty() {
+		var zero T
+		return zero, ErrEmpty
+	}
+
+	topIndex := len(s.store) - 1
+	return s.store[topIndex], nil
+}
+
+func (s *Stack[T]) Pop() (T, error) {
+	if s.Empty() {
+		var zero T
+		return zero, ErrEmpty
+	}
+
+	topIndex := len(s.store) - 1
+	res := s.store[topIndex]
+	s.store = s.store[:topIndex]
+	return res, nil
+}
--- container/stack_test.go
+++ container/stack_test.go
@@ -1,7 +1,54 @@
 package container
 
-import "testing"
+import (
+	"testing"
+
+	testutils "github.com/jesseck3013/dsa/test_utils"
+)
 
 func TestStack(t *testing.T) {
+	t.Run("non-empty stack Top", func(t *testing.T) {
+		s := NewStack[int]()
+		s.Push(1)
+		s.Push(2)
+		s.Push(3)
 
+		want := 3
+		got, err := s.Top()
+		testutils.AssertNoError(t, err)
+		testutils.AssertValue(t, want, got)
+	})
+
+	t.Run("empty stack Top", func(t *testing.T) {
+		s := NewStack[int]()
+
+		_, err := s.Top()
+		testutils.AssertError(t, ErrEmpty, err)
+	})
+}
+
+func TestPop(t *testing.T) {
+	t.Run("non-empty Pop", func(t *testing.T) {
+		s := NewStack[int]()
+		s.Push(1)
+		s.Push(2)
+		s.Push(3)
+
+		want := 3
+		got, err := s.Pop()
+		testutils.AssertNoError(t, err)
+		testutils.AssertValue(t, want, got)
+
+		wantTop := 2
+		gotTop, err := s.Top()
+		testutils.AssertNoError(t, err)
+		testutils.AssertValue(t, wantTop, gotTop)
+	})
+
+	t.Run("empty stack Pop", func(t *testing.T) {
+		s := NewStack[int]()
+
+		_, err := s.Pop()
+		testutils.AssertError(t, ErrEmpty, err)
+	})
 }
