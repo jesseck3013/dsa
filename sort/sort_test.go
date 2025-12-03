19386cb790380bddd525ad62d6bbbff6932eba51
jesseck3013 <jesseck3013@outlook.com>
2025-12-03 16:20:44 +0800

    Updated: use a slice of test cases instead of testing functions

---
+++ sort/sort_test.go
@@ -0,0 +1,53 @@
+package sort
+
+import "testing"
+
+func assertSorted(t *testing.T, s []int, wantLength int) {
+	t.Helper()
+
+	if len(s) != wantLength {
+		t.Errorf("expcted slice length %v, got %v", wantLength, len(s))
+	}
+
+	if len(s) == 0 {
+		return
+	}
+
+	cur := s[0]
+	for _, v := range s {
+		if cur > v {
+			t.Errorf("the given slice is not sorted: %v", s)
+			break
+		}
+	}
+}
+
+type TestCase struct {
+	Name           string
+	Unsorted       []int
+	ExpectedLength int
+}
+
+var testCases = []TestCase{
+	TestCase{"sort unsorted", []int{3, 2, 4, 6, 10}, 5},
+	TestCase{"sort empty", []int{}, 0},
+	TestCase{"sort sorted", []int{1, 2, 3, 4, 5}, 5},
+}
+
+func TestHeapSort(t *testing.T) {
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			HeapSort(testCase.Unsorted)
+			assertSorted(t, testCase.Unsorted, testCase.ExpectedLength)
+		})
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			MergeSort(testCase.Unsorted)
+			assertSorted(t, testCase.Unsorted, testCase.ExpectedLength)
+		})
+	}
+}
